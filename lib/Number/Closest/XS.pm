8c28d8e591f9f786ddb4a12480d9c92f6b1d0c4a
Pavel Shaydo <zwon@cpan.org>
2014-01-11 15:18:19 +0800

    module skeleton

---
+++ lib/Number/Closest/XS.pm
@@ -0,0 +1,43 @@
+package Number::Closest::XS;
+use strict;
+use warnings;
+our $VERSION = "0.01";
+$VERSION = eval $VERSION;
+
+=head1 NAME
+
+Number::Closest::XS - Perl extension ...
+
+=head1 VERSION
+
+0.01
+
+=head1 SYNOPSIS
+
+    use Number::Closest::XS;
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=cut
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+Pavel Shaydo C<< <zwon at cpan.org> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (C) 2014 Pavel Shaydo
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
